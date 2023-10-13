51be34b450854ef0f0705847171f22220720fa77
sharvs04 <142370907+sharvs04@users.noreply.github.com>
2023-10-13 10:48:16 +0800

    update

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+include __DIR__ . "/../src/App/functions.php";
 include __DIR__ . '/../src/App/bootstrap.php';
 
 $app->run();
---
+++ src/App/views/partials/_footer.php
@@ -0,0 +1,8 @@
+<!-- Footer -->
+<footer class="container mx-auto my-12 text-center">
+    &copy; ExpenseTracking
+</footer>
+<!-- End Footer -->
+</body>
+
+</html>

3a97aef1281f5c53c8127b0f2545b10ac679ac1d
sharvs04 <142370907+sharvs04@users.noreply.github.com>
2023-10-16 08:27:15 +0800

    Update _header.php

--- src/App/views/partials/_header.php
+++ src/App/views/partials/_header.php
@@ -9,7 +9,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;700&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="/public/assests/main.css" />
+        <link rel="stylesheet" href="assests/main.css" />
     </head>
 
     <body class="bg-indigo-50 font-['Outfit']">
